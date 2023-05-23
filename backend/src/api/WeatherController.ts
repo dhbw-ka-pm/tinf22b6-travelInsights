af57509cbce009b153ef58ff3ada67021d36af71
Marc Siegfarth <marc.siegfarth@kardex.com>
2023-05-23 19:20:49 +0200

    Adds WeatherController.ts with the GET request "weather/YYYY-MM-DD" for the weather in Karlsruhe

--- backend/src/api/WeatherController.ts
+++ backend/src/api/WeatherController.ts
@@ -1,12 +1,5 @@
 import {Controller, Get, Path, Route} from "tsoa";
-import {WetterTag} from "./wetterHeute";
 import axios from "axios";
-
-// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m
-
-// https://api.open-meteo.com/v1/forecast?latitude=49.01&longitude=8.40&start_date=2021-01-01&end_date=2021-12-31&hourly=temperature_2m
-
-// https://archive-api.open-meteo.com/v1/era5?latitude=52.52&longitude=13.41&start_date=2021-01-01&end_date=2021-12-31&hourly=temperature_2m
 
 @Route('weather')
 export class WeatherController extends Controller {
