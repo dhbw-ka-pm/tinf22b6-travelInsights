e0f0152d9ce802db9d232b4740db69b6a1e87912
Martin <martin.schmidt@freemine.de>
2023-05-31 15:36:36 +0200

    fix(PR): address comments

--- backend/src/api/WeatherController.ts
+++ backend/src/api/WeatherController.ts
@@ -6,8 +6,8 @@ export class WeatherController extends Controller {
     @Get("{date}")
     public async getWeatherOfKarlsruhe(@Path() date : string): Promise<string> {
         return axios
-            .get(`https://api.open-meteo.com/v1/forecast?latitude=49.17&longitude=8.49&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&forecast_days=1&timezone=Europe%2FBerlin&start_date=${date}&end_date=${date}`)
+            .get(`https://api.open-meteo.com/v1/forecast?latitude=49.17&longitude=8.49&daily=temperature_2m_max,temperature_2m_min&forecast_days=1&timezone=Europe%2FBerlin&start_date=${date}&end_date=${date}`)
             .then((data) => data.data)
-            .catch(() => 'Hat net geklappt ._.');
+            .catch(() => Promise.reject("WetterApi ist nicht verfügbar!"));
     }
 }
