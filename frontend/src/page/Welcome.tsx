96127096fd0f1395ec5e40c31b4ce435eb2ae811
Benjamin <bennymp19@gmail.com>
2023-05-31 17:52:07 +0200

    Die Auto-Vervollständigung funktioniert noch nicht!

--- frontend/src/page/Welcome.tsx
+++ frontend/src/page/Welcome.tsx
@@ -1,4 +1,4 @@
-import {ReactElement, useContext} from "react";
+/*import {ReactElement, useContext} from "react";
 import { PageState, PageStateContext } from "../App";
 
 const Welcome = (): ReactElement => {
@@ -10,5 +10,35 @@ const Welcome = (): ReactElement => {
         </>
     )
 }
+
+export default Welcome;
+*/
+import React, { ReactElement, useContext, useState } from "react";
+import { PageState, PageStateContext } from "../App";
+
+const Welcome = (): ReactElement => {
+  const { setState } = useContext(PageStateContext);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearch = () => {
+    // Hier kannst du die Logik für die Suchfunktion implementieren
+    // Verwende die Variable searchTerm für die Suchabfrage
+    console.log("Search term:", searchTerm);
+    // Führe hier den entsprechenden Code aus, um die Suche durchzuführen
+  };
+
+  return (
+    <>
+      Welcome
+      <input type="text" value={searchTerm} onChange={handleInputChange} placeholder="Wohin geht es als nächstes?"/>
+      <button onClick={handleSearch} >Search</button>
+      <button onClick={() => setState(PageState.MAP)}>to Map</button>
+    </>
+  );
+};
 
 export default Welcome;
