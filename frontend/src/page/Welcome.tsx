f928fc9675f070621b3fbedd3cedfe1a7ea83313
Nick Hörner <hoerner@jacob.de>
2023-06-12 18:53:45 +0200

    added Logo and background images, built landing page prototype

--- frontend/src/page/Welcome.tsx
+++ frontend/src/page/Welcome.tsx
@@ -13,32 +13,53 @@ const Welcome = (): ReactElement => {
 
 export default Welcome;
 */
-import React, { ReactElement, useContext, useState } from "react";
-import { PageState, PageStateContext } from "../App";
+import React, {ReactElement, useContext, useEffect, useState} from "react";
+import {PageState, PageStateContext} from "../App";
+import {Button} from "@mui/material";
+import {MapContainer, SVGOverlay, TileLayer} from "react-leaflet";
+import {LatLngBoundsLiteral, LatLngLiteral} from "leaflet";
 
 const Welcome = (): ReactElement => {
-  const { setState } = useContext(PageStateContext);
-  const [searchTerm, setSearchTerm] = useState("");
+    const {setState} = useContext(PageStateContext);
+    const [searchTerm, setSearchTerm] = useState("");
 
-  const handleInputChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
-    setSearchTerm(event.target.value);
-  };
+    const handleInputChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
+        setSearchTerm(event.target.value);
+    };
 
-  const handleSearch = () => {
-    // Hier kannst du die Logik für die Suchfunktion implementieren
-    // Verwende die Variable searchTerm für die Suchabfrage
-    console.log("Search term:", searchTerm);
-    // Führe hier den entsprechenden Code aus, um die Suche durchzuführen
-  };
+    const handleSearch = () => {
+        // Hier kannst du die Logik für die Suchfunktion implementieren
+        // Verwende die Variable searchTerm für die Suchabfrage
+        console.log("Search term:", searchTerm);
+        // Führe hier den entsprechenden Code aus, um die Suche durchzuführen
+    };
 
-  return (
-    <>
-      Welcome
-      <input type="text" value={searchTerm} onChange={handleInputChange} placeholder="Wohin geht es als nächstes?"/>
-      <button onClick={handleSearch} >Search</button>
-      <button onClick={() => setState(PageState.MAP)}>to Map</button>
-    </>
-  );
+    return (
+
+        <>
+
+            {/*Header*/}
+            <div className="headerLandingPage">
+                <img id="logo" src="logoTravelInsights.png"/>
+            </div>
+            <div className="blurFilter">
+                <div className="searchBar">
+
+                    <input type="text" value={searchTerm} onChange={handleInputChange}
+                           placeholder="Wohin geht es als nächstes?"/>
+                    <Button onClick={handleSearch}>Search</Button>
+                </div>
+            </div>
+
+            {/*Mitte*/}
+            <div className="worldBackground"></div>
+            
+            {/*Footer*/}
+            <footer className="footerLandingPage">
+                <Button onClick={() => setState(PageState.MAP)}>to Map</Button>
+            </footer>
+        </>
+    );
 };
 
 export default Welcome;
