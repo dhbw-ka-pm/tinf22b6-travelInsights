790b4e3f7f729f14e70d595e9906531b2e93ca5b
lvejsada <len.vejsada@kardex.com>
2023-06-13 08:49:46 +0200

    You can put the Name of a Country, City or Village into the Search Bar and the Map'll change accordingly

--- frontend/src/page/LeafletMap.tsx
+++ frontend/src/page/LeafletMap.tsx
@@ -1,9 +1,10 @@
-import { LatLngBoundsLiteral, LatLngLiteral } from "leaflet";
-import {ReactElement, useContext, useState} from "react";
+import { useMapEvents } from "react-leaflet";
+import { ReactElement, useContext, useState } from "react";
 import { MapContainer, SVGOverlay, TileLayer } from "react-leaflet";
 import { PageState, PageStateContext } from "../App";
 import { Button, Grid, InputAdornment, Paper, TextField } from "@mui/material";
 import { Search } from "@mui/icons-material";
+import { LatLngLiteral, LatLngBoundsLiteral } from "leaflet";
 
 const LeafletMap = (): ReactElement => {
   const { setState } = useContext(PageStateContext);
@@ -42,53 +43,66 @@ const LeafletMap = (): ReactElement => {
     }
   };
 
-    const position: LatLngLiteral = searchedLocation || { lat: 51.505, lng: - 0.09 }
-    const bounds: LatLngBoundsLiteral = [
-        [51.49, -0.08],
-        [51.5, -0.06],
-    ]
+  const MapEvents = () => {
+    const map = useMapEvents({
+      click: (event) => {
+        const { lat, lng } = event.latlng;
+        setSearchedLocation({ lat, lng });
+        setMapKey((prevKey) => prevKey + 1); // Update mapKey to remount MapContainer
+      }
+    });
 
-    return (
-        <>
-            <Grid container spacing={3}>
-                <Grid item xs={9}>
-                    <MapContainer key={mapKey} center={position} zoom={5} scrollWheelZoom={true}>
-                        <TileLayer
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <SVGOverlay attributes={{ stroke: 'red' }} bounds={bounds}>
-                            <rect x="0" y="0" width="100%" height="100%" fill="blue" />
-                            <circle r="5" cx="10" cy="10" fill="red" />
-                            <text x="50%" y="50%" stroke="white">
-                                text
-                            </text>
-                        </SVGOverlay>
-                    </MapContainer>
-                </Grid>
-                <Grid item xs={3}>
-                    <Paper>
-                        <TextField
-                          fullWidth
-                          value={searchValue}
-                          onChange={(e) => setSearchValue(e.target.value)}
-                          onKeyDown={handleKeyDown}
-                          InputProps={{
-                            endAdornment: (
-                            <InputAdornment position="end">
-                                <Search onClick = {handleSearch}/>
-                            </InputAdornment>
-                            ),
-                          }}
-                        />
-                    </Paper>
-                    <Paper>
-                        <Button onClick={() => setState(PageState.IMPRESSUM)}>Impressum</Button>
-                    </Paper>
-                </Grid>
-            </Grid>
-        </>
-    )
+    return null;
+  };
+
+  const position: LatLngLiteral = searchedLocation || { lat: 51.505, lng: -0.09 };
+  const bounds: LatLngBoundsLiteral = [
+    [51.49, -0.08],
+    [51.5, -0.06],
+  ];
+
+  return (
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={9}>
+          <MapContainer key={mapKey} center={position} zoom={5} scrollWheelZoom={true}>
+            <MapEvents />
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <SVGOverlay attributes={{ stroke: "red" }} bounds={bounds}>
+              <rect x="0" y="0" width="100%" height="100%" fill="blue" />
+              <circle r="5" cx="10" cy="10" fill="red" />
+              <text x="50%" y="50%" stroke="white">
+                text
+              </text>
+            </SVGOverlay>
+          </MapContainer>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper>
+            <TextField
+              fullWidth
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              onKeyDown={handleKeyDown}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <Search onClick={handleSearch} />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Paper>
+          <Paper>
+            <Button onClick={() => setState(PageState.IMPRESSUM)}>Impressum</Button>
+          </Paper>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default LeafletMap;
