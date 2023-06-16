07a03257b1092f01ca5fddc03e654456328962cf
Martin <martin.schmidt@freemine.de>
2023-06-17 01:16:45 +0200

    refactor: GetMediaImage to CardImage

--- backend/src/api/CardImage.ts
+++ backend/src/api/CardImage.ts
@@ -1,15 +1,14 @@
 import axios from 'axios';
 import { Controller, Get, Path, Res, Route, TsoaResponse } from 'tsoa';
 
-@Route('mediaImage')
-export class MediaImage extends Controller {
+@Route('cardImage')
+export class CardImage extends Controller {
   @Get('{place}')
-  public async GetMediaImage(@Path() place: string, @Res() errorResponse: TsoaResponse<400, {
+  public async GetCardImage(@Path() place: string, @Res() errorResponse: TsoaResponse<400, {
     reason: string
   }>): Promise<{ source: string }> {
-    return axios.get(`https://en.wikipedia.org/w/api.php?action=query&titles=${place}&prop=pageimages&format=json&pithumbsize=300`)
+    return axios.get(`https://en.wikipedia.org/w/api.php?action=query&titles=${place}&prop=pageimages&format=json&pithumbsize=500`)
       .then((response) => {
-        console.log(response.data);
         const pages = response.data.query.pages;
         for (let page in pages) {
           return { source: pages[page].thumbnail.source };
