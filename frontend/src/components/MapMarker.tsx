a1cfc3e3856886fb73867d7cf6027159ca108c86
uc-apt <arpit.jha@ucertify.com>
2023-07-02 20:14:58 +0530

    added button and its functionality

--- frontend/src/components/MapMarker.tsx
+++ frontend/src/components/MapMarker.tsx
@@ -5,9 +5,26 @@ import { type City } from '../api.generated';
 export default function MapMarker(props: {
   city: City;
 }): React.ReactElement {
+  // This function => downloads the discription in the text file format
+  const downloadDescriptionFunc = () : void =>{
+    const descString = props.city.shortDescription;
+    const filename = 'description.txt';
+    const element = document.createElement('a');
+    const file = new Blob([descString], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = filename;
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
   return (
     <Marker position={{ lat: props.city.lat, lng: props.city.lng }}>
-      <Popup>{props.city.shortDescription}</Popup>
+      <Popup>
+        <p>
+          {props.city.shortDescription}
+        </p>
+        <button onClick={downloadDescriptionFunc} >Download description</button>
+      </Popup>
     </Marker>
   );
 }
