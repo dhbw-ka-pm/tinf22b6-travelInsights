0a32b20063bc6380ad19f81093d5dd31fa89256e
Nick <115070676+Challenger3010@users.noreply.github.com>
2023-07-07 18:56:49 +0200

    Export data from db (#78)
    * added fav icon
    
    * added fav icon
    
    * added CityInformation.ts to get data as xml
    
    * added CityInformation.ts to get data as xml
    
    ---------
    
    Co-authored-by: JACOB\nhoerner <hoerner@jacob.de>

---
+++ backend/src/api/cityInformation.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Path, Route } from 'tsoa';
+import { AppDataSource } from '../data-source';
+import {City} from "../db/City";
+import {js2xml} from "xml-js";
+
+@Route('cityInformation')
+export class CityInformation extends Controller {
+    @Get('{city}')
+    public async getCityInformation(@Path() city: string): Promise<string> {
+        const countryRepository = AppDataSource.getRepository(City);
+        let currentCity = await countryRepository.find({ where: {name: city.charAt(0).toUpperCase() + city.slice(1)}, select: {name: true, shortDescription: true, longDescription: true}});
+        if (currentCity.length != 0) {
+            return js2xml(currentCity[0], {compact: true, ignoreComment: true, spaces: 4});
+        } else {
+            return Promise.reject('City not found!');
+        }
+    }
+
+}
