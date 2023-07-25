5eef642c123da4fa151989027610d3ecf4488938
Martin <martin.schmidt@freemine.de>
2023-07-25 20:03:08 +0200

    add countryselect

--- frontend/src/routes/LeafletMap.tsx
+++ frontend/src/routes/LeafletMap.tsx
@@ -3,32 +3,29 @@ import { type LatLngLiteral } from 'leaflet';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import {
   Grid,
-  IconButton,
-  InputAdornment,
   LinearProgress,
-  TextField,
   Typography
 } from '@mui/material';
-import { ExploreOff, Search } from '@mui/icons-material';
+import { ExploreOff } from '@mui/icons-material';
 import {
   type City,
   useGetTravelDestinationForCountry
 } from '../api.generated';
 import { useEffect, useState } from 'react';
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
-import { Link, useLoaderData, useSubmit } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import MediaCard from '../components/MediaCard';
 import MapMarker from '../components/MapMarker';
+import CountrySelect from '../components/CountrySelect';
 
 const LeafletMap = (): React.ReactElement => {
   const urlParam = useLoaderData() as string;
-  const submit = useSubmit();
   const { loading: loadingDestinations, data } =
     useGetTravelDestinationForCountry({
       country: urlParam === 'browse' ? 'Worldwide' : urlParam
     });
 
-  const [searchValue, setSearchValue] = useState(
+  const [searchValue] = useState(
     urlParam === 'browse' ? 'Worldwide' : urlParam
   );
 
@@ -52,15 +49,6 @@ const LeafletMap = (): React.ReactElement => {
       setPinData([]);
     }
   }, [urlParam, data, location]);
-
-  const handleKeyDown = (
-    event: React.KeyboardEvent<HTMLInputElement>
-  ): void => {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      submit(null, { method: 'get', action: '/map/' + searchValue });
-    }
-  };
 
   return (
     <>
@@ -89,23 +77,7 @@ const LeafletMap = (): React.ReactElement => {
             style={{ maxHeight: '93vh', overflow: 'auto', flexWrap: 'nowrap' }}
           >
             <Grid item>
-              <TextField
-                fullWidth
-                value={searchValue}
-                onKeyDown={handleKeyDown}
-                onChange={(event) => {
-                  setSearchValue(event.target.value);
-                }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position='end'>
-                      <IconButton component={Link} to={'/map/' + searchValue}>
-                        <Search />
-                      </IconButton>
-                    </InputAdornment>
-                  )
-                }}
-              />
+              <CountrySelect currentCountry={searchValue} />
             </Grid>
             {loadingDestinations ? (
               <Grid item>
--- frontend/src/routes/Welcome.tsx
+++ frontend/src/routes/Welcome.tsx
@@ -1,56 +1,32 @@
-import React, {type ReactElement, useState} from "react";
-import {Box, IconButton, InputAdornment, Paper, TextField, useTheme} from "@mui/material";
-import { Link } from "react-router-dom";
-import { Search } from "@mui/icons-material";
+import React, { type ReactElement } from "react";
+import { Box, Paper, useTheme } from "@mui/material";
+import CountrySelect from "../components/CountrySelect";
 
 const Welcome = (): ReactElement => {
-    const [searchTerm, setSearchTerm] = useState("");
     const theme = useTheme();
-
-    const handleKeyDown = (
-        event: React.KeyboardEvent<HTMLInputElement>
-      ): void => {
-        if (event.key === 'Enter') {
-          window.location.href = '/map/' + (searchTerm === '' ? 'Worldwide' : searchTerm);
-        }
-      };
 
     return (
         <>
             {/* Header */}
-            <Box display='flex' sx={{backgroundColor: theme.palette.primary.main, width:"100%", height:"15vh", justifyContent:"center"}}>
-                <Box component="img" src="logo500.png" alt={"TravelInsights Logo"} sx={{height:"15vh"}}></Box>
+            <Box display='flex' sx={{ backgroundColor: theme.palette.primary.main, width: "100%", height: "15vh", justifyContent: "center" }}>
+                <Box component="img" src="logo500.png" alt={"TravelInsights Logo"} sx={{ height: "15vh" }}></Box>
             </Box>
 
             <div className="blurFilter">
                 <div className="searchBar">
-                    <Paper>
-                        <TextField 
-                            sx={{width:'40vw'}}
-                            value={searchTerm} 
-                            onKeyDown={handleKeyDown}
-                            onChange={(event) => {setSearchTerm(event.target.value)}}
-                            placeholder="Where to next?"
-                            InputProps={{
-                                endAdornment: (
-                                <InputAdornment position='end'>
-                                    <IconButton component={Link} to={'/map/' + (searchTerm === '' ? 'Worldwide' : searchTerm)}>
-                                    <Search />
-                                    </IconButton>
-                                </InputAdornment>
-                                )
-                            }}/>
+                    <Paper sx={{ width: '40vw' }} >
+                        <CountrySelect />
                     </Paper>
                 </div>
             </div>
 
             {/* Mitte */}
-            <div className = "container">
-            <div className="worldBackground"></div>
+            <div className="container">
+                <div className="worldBackground"></div>
             </div>
-            
+
             {/* Footer */}
-            <Box display='flex' sx={{backgroundColor: theme.palette.primary.main, width:"100%", height:"15vh", justifyContent:"center"}}/>
+            <Box display='flex' sx={{ backgroundColor: theme.palette.primary.main, width: "100%", height: "15vh", justifyContent: "center" }} />
         </>
     );
 };
